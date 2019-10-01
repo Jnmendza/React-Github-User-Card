1ed5040db69caca826ee0cd37034b5bceacc97b7
Jnmendza <jonatmendoza@gmail.com>
2019-10-01 14:17:53 -0700

    Getting data from API

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import UserCard from "./components/UserCard"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      user: [],
+    }
+  }
+  
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/jnmendza')
+      .then(res => {
+        console.log(res)
+        console.log(res.data.name)
+        this.setState({
+          user: res.data
+        });
+        console.log(this.state);
+      })
+      .catch(err => console.log(err))
+  }
+  
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (this.state.user !== prevState.user) {
+  //     if (this.state)
+  //   }
+  // }
+  
+  render(){
+    return (
+      <div className="App">
+        <h1 className="header">Header</h1>
+        <p>GitHub Cards</p>
+        <UserCard />
+      </div>
+    )
+  }
 }
 
 export default App;
